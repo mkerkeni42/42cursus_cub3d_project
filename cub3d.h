ad87ede131fd52fff771aa7e5fbaee6d14608864
ykifadji <yanikif30@gmail.com>
2024-03-07 11:00:37 +0100

    push

--- cub3d.h
+++ cub3d.h
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkerkeni <mkerkeni@student.42nice.fr>      +#+  +:+       +#+        */
+/*   By: ykifadji <ykifadji@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 09:00:05 by mkerkeni          #+#    #+#             */
-/*   Updated: 2024/03/07 10:48:10 by mkerkeni         ###   ########.fr       */
+/*   Updated: 2024/03/07 10:58:55 by ykifadji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _CUB3D_H_
 # define _CUB3D_H
-
 
 #include "./Libft/inc/libft.h"
 #include "./Libft/inc/get_next_line.h"
